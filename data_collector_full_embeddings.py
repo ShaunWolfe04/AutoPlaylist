503b5dfe4c594aa37d7ff23a6bf9eb5a797507c8
ejkmvp@umsystem.edu <ejkmvp@umsystem.edu>
2026-05-03 10:20:30 -0500

    added data collector for both small and full embeddings

---
+++ data_collector_full_embeddings.py
@@ -0,0 +1,67 @@
+import csv
+import numpy as np
+
+
+EMBEDDING_SIZE = 500
+
+def extract_features(embeddings):
+    #embeddings shape is (length, 500). We will make 1000 with mean and max (in that order)
+    mean_features = list(np.mean(embeddings, axis=0))
+    max_features = list(np.max(embeddings, axis=0))
+    assert embeddings.shape[1] == EMBEDDING_SIZE
+    assert len(mean_features) == EMBEDDING_SIZE
+    assert len(max_features) == EMBEDDING_SIZE
+
+    comb = mean_features + max_features
+    assert(len(comb) == EMBEDDING_SIZE*2)
+    return comb
+    
+
+#GOAL - generate all_embeddings and all_labels. all_embeddings is num_songs x embeddingdim, and all labels is num_songs x num_playlists
+
+#First, process our labeled data
+f = open("playlist_labels.csv")
+g = csv.reader(f)
+labs = []
+for row in g:
+    labs.append([row[0], row[3], row[4], row[5]])
+r = np.array(labs)
+
+#now, r contains [song file name, label1, label2, .... labeln]
+
+#Using the file names in r, go find the correct embeddings
+embedds = []
+for row in r:
+    filename = row[0]
+    try:
+        l = np.load(f"vectors/embeddings/{filename}.npz")
+    except Exception as e:
+        print(f"failed to load embeddings for file {filename}")
+        print(e)
+        exit()
+
+    #now that we have the file open, pull the embeddings, process them, and add them to an array
+    print(filename)
+    feats = extract_features(l['embedding'])
+    assert len(feats) == EMBEDDING_SIZE * 2
+    embedds.append(feats)
+
+embedds = np.array(embedds)
+assert embedds.shape[1] == EMBEDDING_SIZE * 2
+
+#map likert labels to decimal
+def simple_map(x):
+    mapp = {0: 0.0, 1: 0.33, 2: 0.66, 3: 1.0}
+    return mapp[int(x)]
+vf = np.vectorize(simple_map)
+labels = vf(r[:, 1:])
+
+#now, embedds is [num items, embedding dim]
+
+#now, output to files
+
+
+assert labels.shape[0] == embedds.shape[0]
+
+np.save("all_embeddings.npy", embedds)
+np.save("all_labels.npy", labels)
